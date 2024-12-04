fa7913a2e1020944f14653e192741b509a378607
abdullah-motasim <abdullah.motasim@gmail.com>
2024-12-03 23:16:08 -0500

    Final submission - added discussion, model card, and appendix

--- scripts/05-model_data.R
+++ scripts/05-model_data.R
@@ -47,10 +47,6 @@ match_train_index <- createDataPartition(pakistan_matches$pakistan_win, p = 0.8,
 match_train_data <- pakistan_matches[match_train_index, ]
 match_test_data <- pakistan_matches[-match_train_index, ]
 
-year_train_index <- createDataPartition(pakistan_match_summary$wins, p = 0.8, list = FALSE)
-year_train_data <- pakistan_match_summary[year_train_index, ]
-year_test_data <- pakistan_match_summary[-year_train_index, ]
-
 first_model <-
   stan_glm(
     formula = pakistan_win ~  year + team2 + toss_win,
@@ -60,49 +56,25 @@ first_model <-
     prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_aux = exponential(rate = 1, autoscale = TRUE),
   )
-
-second_model <- stan_glm(
-  formula = wins ~ year + total_matches + tosses_won,  # Predict the number of wins
-  data = year_train_data,  # Your training data
-  family = poisson(link = "log"),  # Use Poisson distribution for count data
-  prior = normal(0, 2.5),  # Prior for coefficients
-  prior_intercept = normal(0, 5),  # Prior for intercept
-  chains = 4,  # Number of chains for MCMC sampling
-  iter = 2000  # Number of iterations
-)
   
 summary(first_model)
-summary(second_model)
 
 #Predict probabilities for test data
 match_test_data$predicted_prob_first <- predict(first_model, newdata = match_test_data, type = "response")
-year_test_data$predicted_prob_second <- predict(second_model, newdata = year_test_data, type = "response")
 
 # Generate predicted classes based on a threshold (e.g., 0.5)
 match_test_data$predicted_class_first <- ifelse(match_test_data$predicted_prob_first > 0.5, 1, 0)
-year_test_data$predicted_class_second <- ifelse(year_test_data$predicted_prob_second > 0.5, 1, 0)
-
 # Evaluate model performance
 confusion_matrix_first <- table(
   Actual = match_test_data$pakistan_win,
   Predicted = match_test_data$predicted_class_first
 )
 
-confusion_matrix_second <- table(
-  Actual = year_test_data$wins,
-  Predicted = year_test_data$predicted_class_second
-)
 print(confusion_matrix_first)
-print(confusion_matrix_second)
 
 accuracy_first <- sum(diag(confusion_matrix_first)) / sum(confusion_matrix_first)
-accuracy_second <- sum(diag(confusion_matrix_second)) / sum(confusion_matrix_second)
 print(paste("Accuracy:", round(accuracy_first, 2)))
-print(paste("Accuracy:", round(accuracy_second, 2)))
 
 #### Save model ####
 saveRDS(first_model, file = "models/win_chance.rds")
-saveRDS(second_model, file = "models/wins_per_year.rds")
 saveRDS(confusion_matrix_first, file = "models/confusion_matrix_win_chance.rds")
-
-
